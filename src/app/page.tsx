e4d41f388eb67ff2985068430fb1a1e8e38f590e
cat terracat <terakot2022@gmail.com>
2025-10-01 11:02:55 +0000

    вот витео  https://vimeo.com/407547341?fl=pl&fe=sh

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -56,16 +56,13 @@ export default function Home() {
 
       <section className="relative bg-background py-16 md:py-24 overflow-hidden">
         <div className="absolute top-0 left-0 w-full h-full bg-black">
-            <video
-                autoPlay
-                loop
-                muted
-                playsInline
-                className="absolute top-1/2 left-1/2 w-auto min-w-full min-h-full max-w-none -translate-x-1/2 -translate-y-1/2 opacity-30 z-0 object-cover"
-            >
-                <source src="https://ruah.uz/images/RGR.mp4" type="video/mp4" />
-                Your browser does not support the video tag.
-            </video>
+          <iframe 
+            src="https://player.vimeo.com/video/407547341?background=1&autoplay=1&loop=1&byline=0&title=0"
+            className="absolute top-1/2 left-1/2 w-auto min-w-full min-h-full max-w-none -translate-x-1/2 -translate-y-1/2 opacity-30 z-0 object-cover"
+            frameBorder="0" 
+            allow="autoplay; fullscreen" 
+            allowFullScreen
+          ></iframe>
           <div className="pixel-overlay" style={{backgroundSize: '3px 3px'}}></div>
         </div>
         <div className="container mx-auto px-4 md:px-6 relative z-10">
@@ -239,3 +236,5 @@ export default function Home() {
     </div>
   );
 }
+
+    
